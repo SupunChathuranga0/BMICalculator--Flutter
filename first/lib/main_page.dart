50673c38b4e0b0bd56078b60cc3ff77b6130b833
Supun Chathuranga <Supunc936@gmail.com>
2024-04-04 19:50:40 +0530

    Set Height,Weight and BMI
    develop the code

--- first/lib/main_page.dart
+++ first/lib/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:first/constant.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -8,13 +9,15 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  int height = 210;
+  int weight = 50;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:     SafeArea(
+      body: SafeArea(
         child:Container( color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: [
             Row(
@@ -29,26 +32,89 @@ class _MainPageState extends State<MainPage> {
                   ),
                 ),
                const Spacer(), 
-                Column(
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                child: Column(
                   children: const[
                     Icon(Icons.female, size: 150,),
                     Text("FeMale"),
                   ],
                 )
-            
-                
+                )                        
               ],
             ),
+
+            SizedBox(height: 50),
             Row(
-              
-            )
-          ],
+              children: [
+                 Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children:  [
+                     const Text("Height"),
+                      Text("$height",
+                       style: kInputLabelColor,
+                          ), 
+                          Row(
+                            children: [
+                              FloatingActionButton(onPressed: (){
+                                setState(() {
+                                  if(height >50)
+                                   height--;
+                                });
+                               
+                                print(height);
+                              }, 
+                              child: Icon(Icons.remove, size: 40,),),
+                              const SizedBox(width: 25),
+                              FloatingActionButton(onPressed:(){
+                                setState(() {
+                                  if(height<210)
+                                   height++;
+                                });
+                                print(height);
+                              }
+                              , child: Icon(Icons.add, size: 40,),)
+                            ]),
+                    ],
+                  ),
+                ),
+               const Spacer(), 
+                   Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children:  [
+                     const Text("Weight"),
+                     const Text("76",
+                       style:kInputLabelColor
+                     ),
+                          Row(
+                            children: [
+                              FloatingActionButton(onPressed:null, child: Icon(Icons.remove, size: 40,),),
+                              const SizedBox(width: 25),
+                              FloatingActionButton(onPressed:null, child: Icon(Icons.add, size: 40,),)
+
+                            ]),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 50,),
+
+            Column(children: [
+              Text("BMI"),
+              Text("22.00", style: kInputLabelColor.copyWith(color: kOutputTextColor,fontSize: 60),)
+            ],)
+            ],
         ),
-      ),)
-     
-    ),
-    )
-;
-    
+      ),
+      )
+    );
+  }
+
+  void onHeighMinus(){
+    print("h m");
   }
 }
